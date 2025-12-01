677ff839c779faef7de05aa2dbdf2a13f16a015d
mmilyass <ilyassmeftah06@gmail.com>
2025-12-01 11:45:15 +0100

    initial commit

---
+++ src/components/navComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from  "../assets/aeternum.png";
+
+export default function NavComponent() {
+  return (
+    <nav>
+      <img src={logo} alt="aeternum logo"/>
+    </nav>
+  );
+}
